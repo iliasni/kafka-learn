a6baecd1e4062a4217575a0af3cff8136c2fd24f
Ilias Nikas <ilnikas@protonmail.com>
2023-08-13 11:04:15 +0300

    Minor Refactoring

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -3,16 +3,18 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Properties;
 
-public class ProducerApp {
-    public static void main(String [] args) throws IOException {
-        // Dataset location
-        final String FILENAME = "src/main/resources/near_earth_comets_asteroids.json";
+public class Producer {
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private static final String filename = "src/main/resources/near_earth_comets_asteroids.json";
 
+    public static void main(String [] args) throws IOException {
         // Configure producer properties
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "localhost:9092"); // Kafka broker addresses
@@ -29,7 +31,7 @@ public class ProducerApp {
 
         // Parse JSON line by line
         ObjectMapper mapper = new ObjectMapper(); // create once, reuse
-        File file = new File(FILENAME);
+        File file = new File(filename);
         JsonParser parser = mapper.getFactory().createParser(file);
         while (parser.nextToken() != JsonToken.END_ARRAY) {
             if (parser.currentToken() == JsonToken.START_OBJECT) {
@@ -43,14 +45,18 @@ public class ProducerApp {
                 // Send message
                 producer.send(record, (metadata, e) -> {
                     if (e != null)
-                        System.out.println("Send failed for record " + record + " :" + e.getMessage());
+                        logger.info("Send failed for record " + record + " :" + e.getMessage());
                 });
-                System.out.println(stellarObject);
+                logger.info("#".repeat(100));
+                logger.info("Key: " + key);
+                logger.info("Value: " + value);
+                logger.info("#".repeat(100));
+                logger.info("\n\n");
             }
         }
         // Close producer
         producer.close();
-        // Close the parser
+        // Close parser
         parser.close();
 
     }
